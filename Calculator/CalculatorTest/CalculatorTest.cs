82156aa3c1cf28567c3a9509a13b6d2f539f1fc5
Björn Persson <bjorn@cedur.se>
2013-07-05 13:04:34 +0200

    First Green test

---
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CalculatorHelper.HandleButton1();
+            Assert.AreEqual(1, CalculatorHelper.Operand1);
+        }
+    }
+
+    public class CalculatorHelper
+    {
+        public static void HandleButton1()
+        {
+            Operand1 = 1;
+        }
+
+        public static int Operand1 { get; set; }
+    }
+}
